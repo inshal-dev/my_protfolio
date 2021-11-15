72beb63350bd96f5963809a6607b46c45bfa2229
Mohammad Inshal <Mdinshal16@gmail.com>
2021-11-15 19:07:56 +0530

    file change

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -13,7 +13,7 @@ export class AboutComponent implements OnInit {
   ngOnInit(): void {
   }
   downloadPdf() {
-    const pdfUrl = '../../assets/Mohammad-Resume.pdf';
+    const pdfUrl = '../../assets/insha;-resume.pdf';
     const pdfName = 'Mohammad-Inshal-Resume';
     FileSaver.saveAs(pdfUrl, pdfName);
   }
